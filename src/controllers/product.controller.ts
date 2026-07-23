994a6f4f132d72c8c41c8c4144f96c8c8a940d7e
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-07-23 19:02:02 +0530

    add deleteProduct and getProducts functions to product controller; update routes to include new endpoints

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,7 +2,7 @@ import { Product } from "../models/product.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const createProduct = asyncHandler( async(req, res) => {
     const { name, slug, description, category } = req.body
@@ -113,7 +113,87 @@ const updateProduct = asyncHandler( async(req, res) => {
     )
 })
 
+const deleteProduct = asyncHandler( async(req, res) => {
+    const { productId } = req.params
+
+    const deletedProduct = await Product.findByIdAndDelete(productId)
+
+    if (!deletedProduct) {
+        throw new ApiError(404, "Product not found")
+    }
+
+    for (const publicId of deletedProduct.imagesPublicId) {
+        deleteFromCloudinary(publicId)
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, null, "Product deleted successfully")
+    )
+})
+
+const getProducts = asyncHandler( async(req, res) => {
+    const { page="1", limit="10", query, sortBy, sortType, category } = req.query
+
+    const pageNumber = Math.max(1, Number(page) || 1);
+    const limitNumber = Math.min(50, Math.max(1, Number(limit) || 10));
+
+    const filter: Record<string, unknown> = {}
+
+    if (query) {
+        filter.$or = [
+            { name: { $regex: query, $options: "i" } },
+            { description: { $regex: query, $options: "i" } }
+        ]
+    }
+
+    if (category) {
+        filter.category = category
+    }
+
+    const allowedSortFields = ["name", "createdAt"] as const;
+
+    const sortField = allowedSortFields.includes(sortBy as any) ? sortBy as string : "createdAt" 
+    const order = sortType === "asc" ? 1 : -1;
+
+
+    const products = await Product.find(filter)
+                    .populate("category")
+                    .sort({ [sortField]: order })
+                    .skip((pageNumber -1) * limitNumber)
+                    .limit(limitNumber)
+
+    const totalProducts = await Product.countDocuments(filter);
+    const totalPages = Math.ceil(totalProducts / limitNumber);
+
+    return res.status(200).json(
+        new ApiResponse(200, {
+            products,
+            page: pageNumber,
+            limit: limitNumber,
+            totalProducts,
+            totalPages
+        }, "Products fetched successfully")
+    )
+})
+
+const getProduct = asyncHandler( async(req, res) => {
+    const { slug } = req.params
+
+    const product = await Product.findOne({ slug }).populate("category")
+
+    if (!product) {
+        throw new ApiError(404, "Product not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, product, "Product fetched successfully")
+    )
+})
+
 export {
     createProduct,
-    updateProduct
+    updateProduct,
+    deleteProduct,
+    getProducts,
+    getProduct
 }
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,19 +3,22 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { verifyAdmin } from "../middlewares/admin.middleware.js";
 import { 
     createProduct,
-    updateProduct
+    updateProduct,
+    deleteProduct,
+    getProducts,
+    getProduct
 } from "../controllers/product.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 // Anyone can view products
-// router.get("/products", getProducts);
-// router.get("/products/:slug", getProduct);
+router.get("/products", getProducts);
+router.get("/products/:slug", getProduct);
 
 // Only admin can modify products
 router.post("/admin/products", verifyJWT, verifyAdmin, upload.array("images", 5), createProduct);
 router.patch("/admin/products/:id", verifyJWT, verifyAdmin, updateProduct);
-// router.delete("/admin/products/:id", verifyJWT, verifyAdmin, deleteProduct);
+router.delete("/admin/products/:id", verifyJWT, verifyAdmin, deleteProduct);
 
 export default router
